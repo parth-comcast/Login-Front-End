848209c9ba98773c44ff9c3d369973cec9d33da9
parth2973 <parth.patel1496@gmail.com>
2018-07-23 09:33:03 -0400

    little more added to register and refactor some code

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
+  styleUrls: [ './register.component.scss' ],
   templateUrl: './register.component.html'
 })
 export class RegisterComponent {
