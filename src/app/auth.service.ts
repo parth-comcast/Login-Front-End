ff782e9591bafc868f14a185adc823a57f899929
parth2973 <parth.patel1496@gmail.com>
2018-07-24 09:22:24 -0400

    post call url change

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -5,16 +5,17 @@ import { Injectable } from '@angular/core';
 
 export class AuthService {
     messages = [];
+    path = 'http://localhost:3000/auth';
 
     constructor( private http: Http ) {}
 
     registerUser(registerData) {
-        this.http.post('http://localhost:3000/register', registerData).subscribe(res => {
+        this.http.post(this.path + '/register', registerData).subscribe(res => {
         });
     }
 
     loginUser(loginData) {
-        this.http.post('http://localhost:3000/login', loginData).subscribe(res => {
+        this.http.post(this.path + '/login', loginData).subscribe(res => {
             localStorage.setItem('token', res.json().token);
             console.log(res);
         });
